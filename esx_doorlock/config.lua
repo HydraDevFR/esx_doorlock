15c92b73ef1cd1db53a74b1b87f657469fed8d2e
devart22 <72200361+devart22@users.noreply.github.com>
2020-10-03 22:51:02 +0800

    Add files via upload

---
+++ esx_doorlock/config.lua
@@ -0,0 +1,135 @@
+Config = {}
+Config.Locale = 'en'
+
+Config.PowerDownCoords ={
+	x = 2832.532, y = 1538.749, z = 24.729
+}
+
+
+Config.DoorList = {
+
+	--Mount Zonah HP
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -440.6439, y = -321.7954, z = 35.06683},
+		textCoords = {x = -440.35, y = -322.47, z = 35.06683},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -442.589, y = -317.0564, z = 35.06619},
+		textCoords = {x = -442.79, y = -316.43, z = 35.06619},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 2.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -448.6458, y = -316.0326, z = 35.06632},
+		textCoords = {x = -448.39, y = -316.64, z = 35.06632},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -449.2249, y = -314.634, z = 35.06632},
+		textCoords = {x = -449.49, y = -314.03	, z = 35.06632},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -447.3418, y = -305.4991, z = 35.07071},
+		textCoords = {x = -447.12, y = -306.2, z = 35.07071},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -451.4746, y = -309.0658, z = 35.06632},
+		textCoords = {x = -451.72, y = -308.37, z = 35.06632},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -452.7051, y = -306.0629, z = 35.06632},
+		textCoords = {x = -452.91, y = -305.37, z = 34.91},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -449.6572, y = -299.8383, z = 35.07071},
+		textCoords = {x = -449.4, y = -300.46, z = 34.91},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -454.9653, y = -300.5515, z = 35.06632},
+		textCoords = {x = -455.24, y = -299.92, z = 35.06632},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -453.0472, y = -291.5856, z = 35.06544},
+		textCoords = {x = -452.87, y = -292.26, z = 34.91},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	{
+		HashKey = 493136920,
+		objCoords  = {x = -457.122, y = -295.3082, z = 35.06632},
+		textCoords = {x = -457.3, y = -294.63, z = 35.06632},
+		authorizedJobs = { 'ambulance' },
+		locked = true,
+		autolock = true, -- Auto Lock doors when you are far from the distance
+		distance = 1.5,
+		size = 1.2
+	},
+	
+	-- End MZ HP
+}
+
